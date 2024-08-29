d940e1d3f1012754ab1776b4516d0886338d1fea
Alexandr Semenec <alexandrsemenec01@gmail.com>
2024-08-29 20:02:26 +0300

    [test modules]

---
+++ helper.js
@@ -0,0 +1,10 @@
+const greeting = () => {
+    // console.log("Hello from helper.js")
+    // console.log(__dirname)
+    // console.log(__filename)
+    // console.log(process.cwd())
+}
+
+module.exports = {
+    greeting
+}
